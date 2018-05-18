90ac9307d46f36b34993a169637346bdf5cb80e4
zelazmon <hyska.monika@gmail.com>
2018-05-18 14:28:21 +0200

    name changes
    changes for: function name, delete _listfrom list name

--- students/hyska_monika/lesson_05_ Lists_list_comprehensions/AgeCalculator.py
+++ students/hyska_monika/lesson_05_ Lists_list_comprehensions/AgeCalculator.py
@@ -2,29 +2,17 @@
 from create_lists import list_elements
 
 
-def check_age(mylist):
-    adults_list = [mylist[x] for x in range(len(mylist)) if mylist[x] >= 18]
-    if len(adults_list) > 0:
-        average_adults = sum(adults_list) / len(adults_list)
+def check_avg_age(ages):
+    adults = [ages[x] for x in range(len(ages)) if ages[x] >= 18]
+    if len(adults) > 0:
+        average_adults = sum(adults) / len(adults)
     else:
         average_adults = "Missing adults."
     print("Average of adults: ", average_adults)
-    child_list = [mylist[x] for x in range(len(mylist)) if mylist[x] < 18]
-    count_child = len(child_list)
+    child = [ages[x] for x in range(len(ages)) if ages[x] < 18]
+    count_child = len(child)
     print("Number of child: ", count_child)
 
-"""
-def age_average(age):
-    adults_list = [mylist[x] for x in range(len(mylist)) if mylist[x] >= 18]
-    if len(adults_list) > 0:
-        adult_avg = sum(adults_list) / len(adults_list)
-    else:
-        adult_avg = "Missing adults."
-    print("Average of adults: ", adult_avg )
-    child_list = [mylist[x] for x in range(len(mylist)) if mylist[x] < 18]
-    count_child = len(child_list)
-    print("Number of child: ", count_child)
-"""
 
 print("Put data for list: number of people, and age for these people.")
-check_age(list_elements())
+check_avg_age(list_elements())
